5165bf31666c8db34270aef173fe841e4817ab23
Mrshen <1220974104@qq.com>
2022-09-20 00:05:23 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-// import CMS from 'netlify-cms-app'
-// // Initialize the CMS object
-// CMS.init()
-// // Now the registry is available via the CMS object.
-// CMS.registerPreviewTemplate('my-template', MyTemplate)
-
-// useEffect(()=>{
-//   let script2 = document.createElement('script');
-//   script2.type = 'text/javascript';
-//   script2.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js';
-//   document.getElementById('root').appendChild(script2);
-//   },[]
-// );
-// componentDidMount(()=> {
-//   let script = document.createElement('script');
-//   script.type = 'text/javascript';
-//   script.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js';
-//   document.getElementById('append').appendChild(script);
-// });
-
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-
-  // Add Netlify Identity script
-  if (typeof window !== "undefined") {
-    // browser code
-    const document = window.document;
-    let script = document.createElement("script");
-    script.innerHTML = `
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-  `;
-    document.head.appendChild(script);
-  }
 
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
@@ -73,9 +33,6 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
